e6bb0fd49b1b75dad33d4b782ef3bf8d08bd1f02
king <knightark@163.com>
2018-09-15 15:39:25 +0800

    first

---
+++ a/1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace a
+{
+    public class Class1
+    {
+        int a = 0;
+        int b = 0;
+    }
+}
---
+++ a/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace a
+{
+    class Class2
+    {
+        int c = 0;
+        int d = 0;
+    }
+}
